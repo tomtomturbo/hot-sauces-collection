caed10bb430d54befca9d0a749c4ed6174215a1f
tomtomturbo <thomasreist@gmail.com>
2022-06-13 09:53:19 +0200

    first commit

---
+++ resources/views/master.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+
+<body>
+    <h1><a href="/test">Hot Sauces</a></h1>
+</body>
+
+</html>
