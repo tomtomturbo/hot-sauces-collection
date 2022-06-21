65e623543ce0fba505ab078f79ed34a4371d7089
tomtomturbo <thomasreist@gmail.com>
2022-06-21 10:53:56 +0200

    saucecontroller added

--- app/Http/Controllers/SauceController.php
+++ app/Http/Controllers/SauceController.php
@@ -3,8 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Sauce;
 
 class SauceController extends Controller
 {
-    //
-}
+public function showAll() {
+
+
+       $sauces = Sauce::all()->sortByDesc('created_at');
+       return view('messages', ['messages' => $sauces]);
+    }}
+
